c619f6a1af7592789dc08895bb16eff60a9012d4
Kenaz Mumanthi <kenazmumanthi75@gmail.com>
2025-12-04 16:57:32 +0300

    Updated UI, added navigation, improved accessories slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,7 @@
 // Placeholder for interactivity if needed
 console.log("WideWeb Cyber website loaded!");
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('show');
+}
